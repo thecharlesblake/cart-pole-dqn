a5c204efb8d0192238923900425c8a50ec996393
thecharlesblake <thecharlesblake@live.co.uk>
2019-09-07 08:49:02 +0100

    Multiple frames fix mem limit

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ hyperparameters = {
     "eps_end": 0.05,
     "eps_decay": 500,
     "target_update": 10,
-    "replay_memory_size": 10000,
+    "replay_memory_size": 100000,
     "state_frame_count": 3
 }
 
@@ -53,28 +53,8 @@ evaluator = evaluator.Evaluator(env, policy_net, device)
 evaluator.run()
 
 # TODO:
-#
-# Main aims:
-# a) verify it's doing what I expect it to!
-# b) improve the performance to get it to pass the test!
-# c) shift the architecture to make it close to the Atari paper
-
-# (a):
-# 1) GIT! :)
-# 2) Understand the loss fn :)
-# 3) Debug statements in relevant places so that I can run through the algo step by step for multiple episodes :)
-# 4) Figure out what I actually want to plot :)
-# 6) Make sure that generally all parts of the program are doing what I expect and the debug mode demonstrates this :)
-
-# TODO: record time taken to get benchmarks too!
-
-# (b): some ideas
-# 1) Get it easily working and transferable to colab :)
-# 1.5) Vertical line when replay memory filled (remove text log), make duration an average of 10-30 :)
-# 2) Record some initial benchmarks (and keep testing against them!!)
-# 3/4 frames stacked
-# not moving image
 # extract the Y channel (luminance) from the RGB frame
+# not moving image
 # similar hyperparameter values (parameterise optimizer) - then experiment
 # scale & square image? (84x84)
 # same CNN arch (see below)
@@ -99,5 +79,5 @@ evaluator.run()
 
 # Benchmarks: (episode, duration, loss)
 # initial-basic-algo: score=37.5, time=210s, learning=q-value plateau after 100 episodes, reward and loss all over the place
-# stacked-frames-1: score=15.45, 354s, time max mem issues, overfitting, q1 going mad & actual reward dropping, some very good episodes though,
-# still suspicious about rand episodes in training being somehow better than eval
+# stacked-frames-1: score=15.45, time=354s, max mem issues, overfitting, q1 going mad & actual reward dropping, some very good episodes though
+# stacked-frames-2: score=61.58, time=432s, fixed max mem issues, much better! Success seems quite binary - v long or v short episodes
